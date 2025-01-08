506fae5e7e6e8cf6e149e7f871978f45b3c45e16
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2025-01-08 23:54:00 +0530

    Create IFstatements.java

---
+++ IFstatements.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IFstatements {
+    public static void main(String[] args){
+        //working with if statements 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter your age:");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        if(age>=18){
+            System.out.print("you can drive");
+        }
+        else{
+            System.out.println("you cannot drive");
+        }
+    }
+    
+}
